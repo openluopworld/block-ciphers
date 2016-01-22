02b4b35b692a3eb1ac867ffd9e36b0b6189b633c
754103342@qq.com <wuhanluop@163.com>
2016-01-22 14:00:41 +0800

    luopeng_only present 64/80 is implementated, 64/128 is not

--- C/simon_64_96&128_c/cipher.c
+++ C/simon_64_96&128_c/cipher.c
@@ -46,8 +46,7 @@ void encryptKeySchedule(const u8 * inputKey, u8 * keys ) {
 
 /*
  * encrypt
- * plainText: plainText[0] is the lower word(y), [1] is the higher(x). 
- *            plainText has just one block.
+ * plainText: plainText has just one block.
  * keys: round keys
  */
 void encrypt(u8 * plainText, const u8 * keys ) {
@@ -64,8 +63,7 @@ void encrypt(u8 * plainText, const u8 * keys ) {
 
 /*
  * decrypt
- * cipherText: cipherText[0] is the lower word(y), [1] is the higher(x).
- *             cipherText has just one block.
+ * cipherText: cipherText has just one block.
  * keys: round keys
  */
 void decrypt(u8 * cipherText, const u8 * keys ) {
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Implementations of block ciphers using C or AVR<br>
 <ul>
 <li><a href="https://eprint.iacr.org/2014/453.pdf">PRIDE 64/128</a></li><br>
 <li><a href="https://eprint.iacr.org/2013/404.pdf">SIMON and SPECK 64/96&128</a></li><br>
-<li><a href="https://www.iacr.org/archive/ches2007/47270450/47270450.pdf">PRESENT 64/80&128</a></li><br>
+<li><a href="https://www.iacr.org/archive/ches2007/47270450/47270450.pdf">PRESENT 64/80</a></li><br>
 <li><a href="#">LBlock 64/80</a></li><br>
 <li><a href="#">Chaskey 128/128</a></li><br>
 <li><a href="#">Chaskey-LTS 128/128</a></li><br>
