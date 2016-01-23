fe3d578be81cdd92336b04d1d916b3df0f8b2f56
luopeng <754103342@qq.com>
2016-01-23 23:59:22 +0800

    luopeng_add sbox

--- C/lblock_64_80/cipher.c
+++ C/lblock_64_80/cipher.c
@@ -8,18 +8,17 @@
 #include "./../common/cipher.h"
 #include "constants.h"
 
-static u8 sbox0[] = {0xc, 0x5, 0x6, 0xb, 0x9, 0x0, 0xa, 0xd, 0x3, 0xe, 0xf, 0x8, 0x4, 0x7, 0x1, 0x2};
-static u8 sbox1[] = {};
-static u8 sbox2[] = {};
-static u8 sbox3[] = {};
-static u8 sbox4[] = {};
-static u8 sbox5[] = {};
-static u8 sbox6[] = {};
-static u8 sbox7[] = {};
-static u8 sbox8[] = {};
-static u8 sbox9[] = {};
-static u8 invsbox[] = {0x5, 0xe, 0xf, 0x8, 0xC, 0x1, 0x2, 0xD, 0xB, 0x4, 0x6, 0x3, 0x0, 0x7, 0x9, 0xA};
+static u8 sbox0[16] = {14, 9, 15, 0, 13, 4, 10, 11, 1, 2, 8, 3, 7, 6, 12, 5};
+static u8 sbox1[16] = {4, 11, 14, 9, 15, 13, 0, 10, 7, 12, 5, 6, 2, 8, 1, 3};
+static u8 sbox2[16] = {1, 14, 7, 12, 15, 13, 0, 6, 11, 5, 9, 3, 2, 4, 8, 10};
+static u8 sbox3[16] = {7, 6, 8, 11, 0, 15, 3, 14, 9, 10, 12, 13, 5, 2, 4, 1};
+static u8 sbox4[16] = {14, 5, 15, 0, 7, 2, 12, 13, 1, 8, 4, 9, 11, 10, 6, 3};
+static u8 sbox5[16] = {2, 13, 11, 12, 15, 14, 0, 9, 7, 10, 6, 3, 1, 8, 4, 5};
+static u8 sbox6[16] = {11, 9, 4, 14, 0, 15, 10, 13, 6, 12, 5, 7, 3, 8, 1, 2};
+static u8 sbox7[16] = {13, 10, 15, 0, 14, 4, 9, 11, 2, 1, 8, 3, 7, 5, 12, 6};
 
+static u8 sbox8[16] = {8, 7, 14, 5, 15, 13, 0, 6, 11, 12, 9, 10, 2, 4, 1, 3};
+static u8 sbox9[16] = {11, 5, 15, 0, 7, 2, 9, 13, 4, 8, 1, 12, 14, 10, 3, 6};
 /*
  * rotate shift right n-bit on x(a 64-bit block)
  */
