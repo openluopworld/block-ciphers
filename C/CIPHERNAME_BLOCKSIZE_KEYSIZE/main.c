bd810759653cda284ab7d093375dd45538fff1fd
luopeng <754103342@qq.com>
2016-01-24 12:02:39 +0800

    luopeng_add common framework

---
+++ C/CIPHERNAME_BLOCKSIZE_KEYSIZE/main.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include "constants.h"
+#include "./../common/cipher.h"
+
+int main () {
+	#if defined(CIPHERNAME_KEY_SIZE) && (CIPHERNAME_KEY_SIZE==80)
+		
+	#else
+	#if defined(CIPHERNAME_KEY_SIZE) && (CIPHERNAME_KEY_SIZE==128)
+		
+	#endif
+	#endif
+	return 0;
+}
