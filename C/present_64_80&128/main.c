b5ad2e470d68deb612884f029009ac6281cb4239
openluopworld <luopengxq@gmail.com>
2016-05-13 10:28:25 +0800

    luopeng_

--- C/present_64_80&128/main.c
+++ C/present_64_80&128/main.c
@@ -3,23 +3,33 @@
 #include "constants.h"
 #include "./../common/cipher.h"
 
+
+#define CASE (1)
+
 int main () {
-	#if defined(PRESENT_KEY_SIZE_80)
+	#if (1==CASE)
 		/*
 		 * PRESENT 64/80
 		 */
-		u8 inputKey[] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
-		u8 keys[PRESENT_BLOCK_SIZE_80/8*(PRESENT_ROUNDS+1)];
+		u8 inputKey[10] = {0x00};
+		u8 keys[PRESENT_KEY_SIZE_80/8*(PRESENT_ROUNDS+1)];
 		u8 plainText[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
 		encryptKeySchedule80(inputKey, keys);
+
+		int i, j;
+		for ( i = 0; i < 16; i++ ) {
+			for ( j = 0; j < 16; j++ ) {
+				printf("%2x ", keys[i*16+j]);
+			}
+			printf("\n");
+		}
 
 		printf("PlainText: %x, %x, %x, %x, %x, %x, %x, %x\n", 
 			plainText[0], plainText[1], plainText[2], plainText[3], 
 			plainText[4], plainText[5], plainText[6], plainText[7]);
 
 		encrypt(plainText, keys);
-		// cipherText is: e72c46c0 f5945049
 		printf("After encryption: %x, %x, %x, %x, %x, %x, %x, %x\n", 
 			plainText[0], plainText[1], plainText[2], plainText[3], 
 			plainText[4], plainText[5], plainText[6], plainText[7]);
